8f12916bbdb7dadaf8f7bcd6a0412530d3f17d49
prosystem-pro <prosystem490@gmail.com>
2025-10-26 17:30:56 -0600

    Despliegue inicial Web Constructora Morgan

---
+++ src/app/Entornos/Entorno.prod.ts
@@ -0,0 +1,6 @@
+export const Entorno = {
+  Produccion: true,
+  ApiUrl: 'https://carritoweb-constructoramorgan-api-production.up.railway.app/api/',
+  // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
+  NombreEmpresa: 'ConstructoraMorgan'
+};
